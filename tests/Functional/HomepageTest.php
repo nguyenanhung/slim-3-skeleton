a583bdb9a4436f84df782086e6aa4fa53ece97ca
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-24 00:21:56 +0700

    TestCase

--- tests/Functional/HomepageTest.php
+++ tests/Functional/HomepageTest.php
@@ -12,19 +12,8 @@ class HomepageTest extends BaseTestCase
         $response = $this->runApp('GET', '/');
 
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('SlimFramework', (string)$response->getBody());
-        $this->assertNotContains('Hello', (string)$response->getBody());
-    }
-
-    /**
-     * Test that the index route with optional name argument returns a rendered greeting
-     */
-    public function testGetHomepageWithGreeting()
-    {
-        $response = $this->runApp('GET', '/name');
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertContains('Hello name!', (string)$response->getBody());
+        $this->assertContains('Custom by HungNg', (string) $response->getBody());
+        $this->assertNotContains('Hello', (string) $response->getBody());
     }
 
     /**
@@ -35,6 +24,6 @@ class HomepageTest extends BaseTestCase
         $response = $this->runApp('POST', '/', ['test']);
 
         $this->assertEquals(405, $response->getStatusCode());
-        $this->assertContains('Method not allowed', (string)$response->getBody());
+        $this->assertContains('Method not allowed', (string) $response->getBody());
     }
 }
