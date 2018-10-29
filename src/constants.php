0049c828a728102dead772fc6e04b41fce8e61a7
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-29 16:11:56 +0700

    Update Slim3

--- src/constants.php
+++ src/constants.php
@@ -6,4 +6,40 @@
  * Date: 10/29/18
  * Time: 15:45
  */
-defined('PROJECT_ENV') OR exit('production');
+/*
+ *---------------------------------------------------------------
+ * APPLICATION ENVIRONMENT
+ *---------------------------------------------------------------
+ *
+ * You can load different configurations depending on your
+ * current environment. Setting the environment also influences
+ * things like logging and error reporting.
+ *
+ * This can be set to anything, but default usage is:
+ *
+ *     development
+ *     testing
+ *     production
+ *
+ * NOTE: If you change these, also change the error_reporting() code below
+ */
+defined('ENVIRONMENT') OR define('ENVIRONMENT', 'production');
+
+/**
+ * Exit Status Codes
+ *
+ * @see http://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+ * @see http://www.gsp.com/cgi-bin/man.cgi?section=3&topic=sysexits
+ * @see http://tldp.org/LDP/abs/html/exitcodes.html
+ */
+defined('EXIT_SUCCESS') OR define('EXIT_SUCCESS', 0); // no errors
+defined('EXIT_ERROR') OR define('EXIT_ERROR', 1); // generic error
+defined('EXIT_CONFIG') OR define('EXIT_CONFIG', 3); // configuration error
+defined('EXIT_UNKNOWN_FILE') OR define('EXIT_UNKNOWN_FILE', 4); // file not found
+defined('EXIT_UNKNOWN_CLASS') OR define('EXIT_UNKNOWN_CLASS', 5); // unknown class
+defined('EXIT_UNKNOWN_METHOD') OR define('EXIT_UNKNOWN_METHOD', 6); // unknown class member
+defined('EXIT_USER_INPUT') OR define('EXIT_USER_INPUT', 7); // invalid user input
+defined('EXIT_DATABASE') OR define('EXIT_DATABASE', 8); // database error
+defined('EXIT__AUTO_MIN') OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
+defined('EXIT__AUTO_MAX') OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
