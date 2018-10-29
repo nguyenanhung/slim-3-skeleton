46c0c0247bb869ec442e5d03729b625f367433be
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-29 16:26:37 +0700

    Update Source

--- src/routes.php
+++ src/routes.php
@@ -6,7 +6,6 @@ use Slim\Http\Response;
 // Routes
 $app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
-
     $this->logger->info("Slim-Skeleton '/' route" . json_encode($request->getQueryParams()));
 
     // Render index view
